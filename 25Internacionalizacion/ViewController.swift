82fa384c9aff4fbf4d57279198ea2b4d1123b1e2
mbeascoa <miguel.beascoabeastek.net>
2021-09-08 09:33:03 +0200

    Initial Commit

---
+++ 25Internacionalizacion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  25Internacionalizacion
+//
+//  Created by mbeascoa on 8/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
